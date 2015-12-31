260ba7413e3b21127e28c0fe6fcf6e8a7f76e89e
Jamie <jamie@mbpro.local>
2015-12-31 16:10:48 +0100

    commit

--- lib/functions.py
+++ lib/functions.py
@@ -156,9 +156,9 @@ def uploadNaarGitHub(file):
 def geefDatumEnTijd():
     return str(datetime.now())[0:19]
 
-def leesXMLUit(xml, access_mode, root_element):
+def leesXMLConfig(xml, access_mode, agent):
     f = open(xml, access_mode) # open xml file
     config = xmltodict.parse(f.read())
-    return config[root_element]["algemeen"], config[root_element] # geef algemene config en hele config terug
+    return config["monitoringtool"]["algemeen"], config["monitoringtool"][agent] # geef algemene config en hele config terug
 
 uploadNaarGitHub(__file__)
